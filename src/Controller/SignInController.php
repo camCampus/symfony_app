d5651dcc40442333e2180179f0ef180bb660305e
camCampus <camille.bochard@le-campus-numerique.fr>
2023-03-21 17:09:16 +0100

    Add user loggin form | Start sign up form

---
+++ src/Controller/SignInController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SignInType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignInController extends AbstractController
+{
+    #[Route('/sign', name: 'app_sign_in')]
+    public function index(): Response
+    {
+        $form = $this->createForm(SignInType::class);
+
+        return $this->render('sign_in/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
